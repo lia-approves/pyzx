59d4c33e0a981981e81171d6ef1de8a9e6af9d5b
Lia Yeh <lia_yeh@ucsb.edu>
2019-08-14 18:59:24 -0400

    def QiskitTranspilerPass

---
+++ pyzx/circuit/pyzxtranspilerpass.py
@@ -0,0 +1,72 @@
+# PyZX - Python library for quantum circuit rewriting 
+#        and optimisation using the ZX-calculus
+# Copyright (C) 2018 - Aleks Kissinger and John van de Wetering
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import re
+from re import finditer
+import pyzx
+from pyzx.circuit.qiskitqasmparser import QiskitQASMParser
+
+
+def QiskitTranspilerPass(qasm):
+    """
+    Class for parsing OPENQASM source files from a Qiskit QuantumCircuit,
+    optimizing within PyZX, and converting back to OPENQASM that can be
+    reconstructed into a Qiskit QuantumCircuit.
+    """
+    # Call to a parent class of QASMParser to parse Qiskit's OPENQASM
+    p = QiskitQASMParser()
+    circ_list, whichpyzx = p.qiskitparse(qasm)
+    graph_list = [circ_list[w].to_graph() for w in whichpyzx]
+    [pyzx.full_reduce(g) for g in graph_list]
+    pyzx_circ_list = [pyzx.extract.streaming_extract(g) for g in graph_list]
+    # Nested for-while-for loop to continue extracting until it can't
+    for i in range(len(pyzx_circ_list)):
+        prev = pyzx_circ_list[i]
+        next = pyzx.extract.streaming_extract(graph_list[i])
+        while prev.gates != next.gates:
+            for j in range(len(next.gates))[::-1]:
+                pyzx_circ_list[i].add_gate_to_front(next.gates[j])
+            prev = next
+            next = pyzx.extract.streaming_extract(graph_list[i])
+    pyzx_circ_list = [pyzx.optimize.basic_optimization(new_c.to_basic_gates()) for new_c in pyzx_circ_list]
+    pyzx_qasm = [new_c.to_basic_gates().to_qasm() for new_c in pyzx_circ_list]
+    # Verify with compare_tensor that all PyZX optimizations correctly simplify
+    passedAll = True
+    for i in range(len(pyzx_circ_list)):
+        try:
+            assert(pyzx.compare_tensors(pyzx_circ_list[i], circ_list[whichpyzx[i]]))
+        except AssertionError:
+            passedAll = False
+    if not passedAll:
+        return qc
+    # Ignore all register declarations and map registers back to the input qasm
+    pyzx_qasm = ["\n".join(['' if line.startswith("qreg") else line for line in circ.splitlines()[2:]]) for circ in pyzx_qasm]
+    for i in range(len(pyzx_qasm)):
+        circ_list[whichpyzx[i]] = pyzx_qasm[i]
+    sorted_registers = sorted(p.registers.items(), key=lambda x: x[1][0])
+    poss = [m.start() for m in re.finditer('q\[', qasm_string)]
+    registered_qasm = ''
+    prev_pos = 0
+    for pos in poss:
+        registered_qasm += qasm_string[prev_pos:pos]
+        prev_pos = pos+qasm_string[pos:].find(']')
+        id = int(qasm_string[pos+2:prev_pos])
+        leq_list = list(filter(lambda x: (x[1][0] <= id), sorted_registers))
+        registered_qasm += leq_list[-1][0] + '[' + str(id - leq_list[-1][1][0])
+    registered_qasm += qasm_string[prev_pos:]
+    return registered_qasm
